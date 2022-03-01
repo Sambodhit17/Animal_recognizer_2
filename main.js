9d8dbe664f8254fe4ada8dbdb08a5eba11cef85f
Sambodhit17 <92640348+Sambodhit17@users.noreply.github.com>
2022-03-01 20:25:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function start() {
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier(modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
